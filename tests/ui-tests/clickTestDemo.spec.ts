6951bb027ddec5756ebf7b04fa8a844f964b1abb
Srinivasaimandi <Srinivasaimandi@users.noreply.github.com>
2025-06-15 11:26:59 +0530

    Update clickTestDemo.spec.ts

--- tests/ui-tests/clickTestDemo.spec.ts
+++ tests/ui-tests/clickTestDemo.spec.ts
@@ -71,3 +71,65 @@ test("radio button demo test", {
     const message = await page.locator("xpath=.//label[text()='Male']/following::p").first().textContent();
     await expect(message).toBe("Radio button 'Male' is checked");
 })
+
+test("mouse hover test", {
+    tag: "@actions-test @reg",
+    annotation: {
+        type: "test",
+        description: "hover test on a button for change in color",
+    }
+}, async function () {
+    await page.goto("https://www.lambdatest.com/selenium-playground/hover-demo");
+    await page.waitForURL("**/hover-demo");
+    let hoverElement = await page.locator("xpath=.//div[h2[text()='CSS Hover Effects on Button']]/div[1]/div[1]");
+    const bgColorBeforeHover = await hoverElement.evaluate(el => window.getComputedStyle(el).getPropertyValue('background-color'));  
+    console.log("Background color before hover:", bgColorBeforeHover);
+    expect(bgColorBeforeHover).toBe("rgb(40, 167, 69)");
+    await hoverElement.hover();
+    await page.waitForTimeout(2000);
+    const bgColorAfterHover = await hoverElement.evaluate(el => window.getComputedStyle(el).getPropertyValue('background-color'));
+    console.log("Background color after hover:", bgColorAfterHover);
+    expect(bgColorAfterHover).toBe("rgb(255, 255, 255)");
+})
+
+/**
+ * assertions pending
+ */
+test.skip("auto scrolling in playwright", {
+    tag: "@actions-test @reg",
+    annotation: {
+        type: "test",
+        description: "scroll to a specific element without using scroll utils"
+    }
+}, async function () {
+    await page.goto("https://www.lambdatest.com/selenium-playground/hover-demo");
+    await page.waitForURL("**/hover-demo");
+    // await page.screenshot({path: "baseline.png"});
+    await page.locator(".s__column3 img").hover();
+    // await expect(page).not.toHaveScreenshot("baseline.png");
+})
+
+test("simple form test using co-ordinates", {
+    tag: "@actions-test @reg",
+    annotation: {
+        type: "test",
+        description: "click test using co-ordinates",
+    }
+}, async function () {
+    let sample_message: string = "Hello, LambdaTest!";
+    await page.goto("https://www.lambdatest.com/selenium-playground/simple-form-demo");
+    await page.waitForURL("**/simple-form-demo");
+    const element = page.locator("#user-message").first();
+    const box = await element.boundingBox();
+
+    if (box) {
+        await page.mouse.click(box.x + 10, box.y + 10);
+        await element.fill("Clicked using coordinates!");
+        await page.locator("#showInput").click();
+        const message = await page.locator("#message").first().textContent();
+        await expect(message).toBe("Clicked using coordinates!");
+    } else {
+        throw new Error("Element bounding box not found");
+    }
+}
+)
